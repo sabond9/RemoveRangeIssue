341820a3ef2d6e56aec7072b0d6f97f722e281ef
sabond9 <sabond9@gmail.com>
2019-12-20 16:18:13 +0200

    main project

---
+++ WebApplication2/Foo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public interface IFoo
+    {
+        Task Update(Order order);
+    }
+
+    public class Foo : IFoo
+    {
+        private MyContext context;
+
+        public Foo(MyContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task Update(Order order)
+        {
+            context.Orders.RemoveRange(context.Orders.Where(r => r.CustomerID == 100));
+            context.Orders.RemoveRange(context.Orders.Where(r => r.CustomerID == 120));
+            order.EmployeeID = 2;
+            context.Update(order);
+            await context.SaveChangesAsync();
+        }
+    }
+}
